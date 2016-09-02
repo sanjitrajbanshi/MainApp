12979134680dd3343e6ff73260e012dac2bcf177
sanjitrajbanshi <sanjit.rajbanshi@gmail.com>
2016-09-02 18:39:13 -0500

    api

---
+++ WebApplication1/Scripts/gitApi.js
@@ -0,0 +1,38 @@
+﻿
+
+$(document).ready(function () {    
+    hideControl();
+        $('#btnSubmit').click(function () {
+        $('#repo').empty();
+        $('#txtDetails').val("");
+        var userName = $('#txtUser').val();
+        
+            var url = "https://api.github.com/users/" + userName + "/repos"            
+            $.get(url, function (data, status) {
+                    $.each(data, function (key, value) {
+                        $('#repo').append($('<option>', { value: value.name, text: value.name }));
+                    });
+                    $('#repo').show();
+                    $('#lblgit').show();
+                    $('#repo').change(function () {
+                        var a = $('#repo').val();
+                        $.each(data, function (key, value) {
+                            if (a == value.name) {                               
+                                $('#txtDetails').val(JSON.stringify(value));
+                            }
+                        });
+                        $('#txtDetails').show();
+                        $('#lblDetails').show();
+                    })             
+            });
+        })      
+});
+
+function hideControl()
+{
+    $('#repo').hide();
+    $('#lblgit').hide();
+    $('#lblDetails').hide();
+    $('#txtDetails').hide();
+
+}
