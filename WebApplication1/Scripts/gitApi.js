9919a7ce64426391db97575f9634cde0e77ff4b2
sanjitrajbanshi <sanjit.rajbanshi@gmail.com>
2016-09-02 22:06:41 -0500

    update

--- WebApplication1/Scripts/gitApi.js
+++ WebApplication1/Scripts/gitApi.js
@@ -1,38 +1,45 @@
 ﻿
 
-$(document).ready(function () {    
+$(document).ready(function () {
     hideControl();
-        $('#btnSubmit').click(function () {
+    //on submit button click call the list of all github repos displays on combo box
+    $('#btnSubmit').click(function () {
         $('#repo').empty();
         $('#txtDetails').val("");
         var userName = $('#txtUser').val();
-        
-            var url = "https://api.github.com/users/" + userName + "/repos"            
-            $.get(url, function (data, status) {
-                    $.each(data, function (key, value) {
-                        $('#repo').append($('<option>', { value: value.name, text: value.name }));
-                    });
-                    $('#repo').show();
-                    $('#lblgit').show();
-                    $('#repo').change(function () {
-                        var a = $('#repo').val();
-                        $.each(data, function (key, value) {
-                            if (a == value.name) {                               
-                                $('#txtDetails').val(JSON.stringify(value));
-                            }
-                        });
-                        $('#txtDetails').show();
-                        $('#lblDetails').show();
-                    })             
+        var url = "https://api.github.com/users/" + userName + "/repos";
+        //call the github api list of repository for the given user
+        var result = $.get(url);
+        result.success(function (data) {
+            $.each(data, function (key, value) {
+                $('#repo').append($('<option>', { value: value.key, text: value.name }));
             });
-        })      
+            $('#repo').show();
+            $('#lblgit').show();
+            $('#repo').change(function () {
+                var repoName = $('#repo').val();
+                $.each(data, function (key, value) {
+                    if (repoName == value.name) {
+                        $('#txtDetails').val(JSON.stringify(value));
+                    }
+                });
+                $('#txtDetails').show();
+                $('#lblDetails').show();
+            });
+        });
+
+        result.error(function (jqXHR, textStatus, errorThrown) {
+            alert("Enter valid user-"+errorThrown);
+        });
+
+    });
+
+
 });
+    function hideControl() {
+        $('#repo').hide();
+        $('#lblgit').hide();
+        $('#lblDetails').hide();
+        $('#txtDetails').hide();
 
-function hideControl()
-{
-    $('#repo').hide();
-    $('#lblgit').hide();
-    $('#lblDetails').hide();
-    $('#txtDetails').hide();
-
-}
+    }
